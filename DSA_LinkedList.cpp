06525ccf74b722d16873324ab181379b9ca94890
Shawn Jumawan <shawnmichaeljumawan@gmail.com>
2023-01-27 21:27:24 +0800

    Update CPP file

--- DSA_LinkedList.cpp
+++ DSA_LinkedList.cpp
@@ -10,7 +10,36 @@ struct node
     struct node *prev;
 };
 
+class linked_list
+{
+    private:
+        node *head, *tail;
+    public:
+        linked_list()
+        {
+            head = NULL;
+            tail = NULL;
+        }
 
+        void add_node(int n)
+        {
+            node *tmp = new node;
+            tmp -> data = n;
+            tmp -> next = NULL;
+
+            if (head == NULL)
+            {
+                head = tmp;
+                tail = tmp;
+            }
+            else
+            {
+                tail -> next = tmp;
+                tail = tail -> next;
+            }
+            
+        }
+};
 
 
 int main(int argc, char const *argv[])
@@ -18,7 +47,7 @@ int main(int argc, char const *argv[])
     int arry1[7];
     int arry2[7];
 
-    
+
 
     
     return 0;
