8219528612920a7df89aa7428e35d10bdc68cf5e
Shawn Jumawan <shawnmichaeljumawan@gmail.com>
2023-01-27 22:23:10 +0800

    Update CPP file

--- DSA_LinkedList.cpp
+++ DSA_LinkedList.cpp
@@ -3,26 +3,74 @@
 
 using namespace std;
 
-struct node
+struct Node
 {
     int data;
-    struct node *next;
+    Node *next;
+    Node *prev;
 };
+
+class linked_list
+{
+    Node *head, *tail;
+
+public:
+    linked_list()
+    {
+        head = NULL;
+        tail = NULL;
+    }
+
+    void insert(int value)
+    {
+        Node *temp = new Node;
+        temp->data = value;
+        temp->next = NULL;
+        temp->prev = NULL;
+
+        if (head == NULL)
+        {
+            head = temp;
+            tail = temp;
+            temp = NULL;
+        }
+        else
+        {
+            tail->next = temp;
+            temp->prev = tail;
+            tail = temp;
+        }
+        
+    }
+
+    void display() 
+    {
+        Node *temp = new Node;
+        temp = head;
+        while (temp != NULL) 
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+
+};
+
 
 
 int main(int argc, char const *argv[])
 {
-    List list1;
-    List list2;
+    linked_list list1;
+    linked_list list2;
 
-    cout << "Enter elements for first list (maximum of 7 elements):" << endl;
+    cout << "Enter elements for first list (maximum of 7 elements): \n" ;
     for (int i = 0; i < 7; i++) {
         int element;
         cin >> element;
-        list1.addNode(element);
+        list1.insert(element);
     }
 
+    list1.display();
 
-    
     return 0;
 }
